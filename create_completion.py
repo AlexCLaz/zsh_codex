607a5465300304b5af4d928daad80d02b8af81ff
Alex <alex.lazarini@otima.digital>
2023-12-21 11:17:49 -0300

    fix missing file

--- create_completion.py
+++ create_completion.py
@@ -32,8 +32,8 @@ def create_template_ini_file():
         print('OpenAI API config file created at {}'.format(API_KEYS_LOCATION))
         print('Please edit it and add your organization ID and secret key')
         print('If you do not yet have an organization ID and secret key, you\n'
-               'need to register for OpenAI Codex: \n'
-                'https://openai.com/blog/openai-codex/')
+               'need to register for OpenAI: \n'
+                'https://platform.openai.com/api-keys')
         sys.exit(1)
 
 
